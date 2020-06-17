fb05eb37e12676f9b9f34b4c6d16b719b74182ae
Ryan Turner <rturner@twitter.com>
2020-06-16 17:48:08 -0700

    cleaning up prepare upload

--- prepare_upload.sh
+++ prepare_upload.sh
@@ -3,9 +3,10 @@
 set -ex
 set -o pipefail
 
+# Input args
 CODE_DIR=$1
-REQ_FILE=$2
 
+# Setup vars
 NAME=upload_$(basename $CODE_DIR)
 # Eliminate final slash
 CODE_DIR=$(dirname $CODE_DIR)/$(basename $CODE_DIR)
@@ -13,14 +14,20 @@ CODE_DIR=$(dirname $CODE_DIR)/$(basename $CODE_DIR)
 # Copy in provided files
 cp -r -n $CODE_DIR ./$NAME
 
+# Make a blank req file if none provided
+REQ_FILE=CODE_DIR/requirements.txt
+touch $REQ_FILE
+
 # Download all the wheels/tar balls with our docker as the target
 pip download -r $REQ_FILE -d ./$NAME --python-version 37 --implementation cp --platform manylinux1_x86_64 --abi cp37m --no-deps
 
-# Test zip does not exist yet
+# Test zip does not exist yet to avoid clobber
 ! test -f $NAME.zip
 
+# Build the zip with correct directory structure
 (cd $NAME && zip -r ../$NAME.zip ./*)
 
+# Display final output for user at end
 set +x
 
 echo "----------------------------------------------------------------"
